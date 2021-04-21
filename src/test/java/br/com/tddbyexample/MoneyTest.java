0a4e5ed3296fc5691fc84d2380ccdbece84b5bbe
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2021-04-20 22:59:44 -0300

    Chapter 7.

--- src/test/java/br/com/tddbyexample/MoneyTest.java
+++ src/test/java/br/com/tddbyexample/MoneyTest.java
@@ -25,5 +25,6 @@ public class MoneyTest {
         Assertions.assertFalse(new Dollar(5).equals(new Dollar(6)));
         Assertions.assertTrue(new Franc(5).equals(new Franc(5)));
         Assertions.assertFalse(new Franc(5).equals(new Franc(6)));
+        Assertions.assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 }
