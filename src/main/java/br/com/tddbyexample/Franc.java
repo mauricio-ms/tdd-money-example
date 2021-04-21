91f0ff1b16b472cc9a310789a7b3fdd3f6a423d5
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2021-04-20 21:51:20 -0300

    Chapter 5.

---
+++ src/main/java/br/com/tddbyexample/Franc.java
@@ -0,0 +1,20 @@
+package br.com.tddbyexample;
+
+public class Franc {
+
+    private final int amount;
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Franc dollar = (Franc) object;
+        return amount == dollar.amount;
+    }
+}
--- src/test/java/br/com/tddbyexample/MoneyTest.java
+++ src/test/java/br/com/tddbyexample/MoneyTest.java
@@ -3,13 +3,20 @@ package br.com.tddbyexample;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class DollarTest {
+public class MoneyTest {
 
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         Assertions.assertEquals(new Dollar(10), five.times(2));
         Assertions.assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        Assertions.assertEquals(new Franc(10), five.times(2));
+        Assertions.assertEquals(new Franc(15), five.times(3));
     }
 
     @Test
